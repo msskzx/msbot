077e75a6b6497f7e94f14fdf212d2e62aded33c7
Muhammad Khattab <msskmohammed96@gmail.com>
2017-04-25 19:25:50 +0200

    activity index

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var express = require("express");
 var request = require("request");
 var bodyParser = require("body-parser");
 
+const msAPI = 'http://35.160.199.92:8080'
+
 var app = express();
 app.set('port', process.env.PORT || 5000);
 app.use(bodyParser.urlencoded({
@@ -9,16 +11,6 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(bodyParser.json());
 
-// Server index page
-app.get("/", function(req, res) {
-    res.status(200).json({
-        status: 'success',
-        message: "why look at this! it's working!"
-    });
-});
-
-// Facebook Webhook
-// Used for verification
 app.get("/webhook", function(req, res) {
     if (req.query["hub.verify_token"] === process.env.VERIFY_TOKEN) {
         res.status(200).send(req.query["hub.challenge"]);
@@ -27,14 +19,9 @@ app.get("/webhook", function(req, res) {
     }
 });
 
-// All callbacks for Messenger will be POST-ed here
 app.post("/webhook", function(req, res) {
-    // Make sure this is a page subscription
     if (req.body.object == "page") {
-        // Iterate over each entry
-        // There may be multiple entries if batched
         req.body.entry.forEach(function(entry) {
-            // Iterate over each messaging event
             entry.messaging.forEach(function(event) {
                 if (event.postback) {
                     processPostback(event);
@@ -43,9 +30,19 @@ app.post("/webhook", function(req, res) {
                 }
             });
         });
-
         res.sendStatus(200);
     }
+});
+
+// 404 for any other route
+app.use(function(req, res, next) {
+    if (!res.headersSent) {
+        res.status(404).json({
+            status: 'fail',
+            message: 'Not found'
+        });
+    }
+    next();
 });
 
 function processPostback(event) {
@@ -53,8 +50,6 @@ function processPostback(event) {
     var payload = event.postback.payload;
 
     if (payload === "Greeting") {
-        // Get user's first name from the User Profile API
-        // and include it in the greeting
         request({
             url: "https://graph.facebook.com/v2.6/" + senderId,
             qs: {
@@ -90,12 +85,22 @@ function processMessage(event) {
         var message = event.message;
         var senderId = event.sender.id;
 
-        // You may get a text or attachment but not both
         if (message.text) {
-            var formattedMsg = message.text.toUpperCase().trim();
-            sendMessage(senderId, {
-                text: formattedMsg
-            });
+            var formattedMsg = message.text.toLowerCase().trim();
+            switch (messageText) {
+                case 'activities':
+                    activityIndex(senderID);
+                    break;
+
+                case 'businesses':
+                    businessIndex(senderID);
+                    break;
+
+                default:
+                    sendMessage(senderID, {
+                        text: "why you no use commands that I know?"
+                    });
+            }
         } else if (message.attachments) {
             sendMessage(senderId, {
                 text: "bitte?"
@@ -125,7 +130,27 @@ function sendMessage(recipientId, message) {
     });
 }
 
+function activityIndex(senderId) {
+    request({
+        url: msAPI + "/activities/page/0",
+        method: "GET",
+    }, function(errors, response, body) {
+        if (errors) {
+            console.log("Error sending message: " + response.errors);
+        } else {
+            console.log(response);
+            console.log(body);
+            for (var i = 0; i < body.data.activities.length && i < 5; i++)
+                sendMessage(senderId, { text: body.data.activities[i].name });
+        }
+    });
+}
+
+function businessIndex(senderId) {
+
+}
+
 app.listen(app.get('port'), function() {
-    console.log('run, bot... run');
+    console.log('run, bot... run...');
     console.log(app.get('port'));
 });
